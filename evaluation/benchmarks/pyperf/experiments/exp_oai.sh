c717b7fbf43bd0d3c5b45033de30c5aaf7e59461
manishsshettym <manishs@berkeley.edu>
2025-05-03 01:31:02 +0000

    oai exp update

--- evaluation/benchmarks/pyperf/experiments/exp_oai.sh
+++ evaluation/benchmarks/pyperf/experiments/exp_oai.sh
@@ -10,20 +10,20 @@ set -eo pipefail
 
 
 # L = 50, k = 8, temp = N/A
-# ./evaluation/benchmarks/pyperf/scripts/run_infer.sh --model-config llm.eval_o4-mini --commit-hash HEAD --agent CodeActAgent --num-workers 60 --n-runs 8 --max-iter 50
-# docker container prune -f
+./evaluation/benchmarks/pyperf/scripts/run_infer.sh --model-config llm.eval_o4-mini --commit-hash HEAD --agent CodeActAgent --num-workers 80 --n-runs 8 --max-iter 50
+docker container prune -f
 
 # L = 100, k = 4, temp = N/A
-# ./evaluation/benchmarks/pyperf/scripts/run_infer.sh --model-config llm.eval_o4-mini --commit-hash HEAD --agent CodeActAgent --num-workers 60 --n-runs 4 --max-iter 100
-# docker container prune -f
+./evaluation/benchmarks/pyperf/scripts/run_infer.sh --model-config llm.eval_o4-mini --commit-hash HEAD --agent CodeActAgent --num-workers 80 --n-runs 4 --max-iter 100
+docker container prune -f
 
 # L = 200, k = 2, temp = N/A
-# ./evaluation/benchmarks/pyperf/scripts/run_infer.sh --model-config llm.eval_o4-mini --commit-hash HEAD --agent CodeActAgent --num-workers 60 --n-runs 2 --max-iter 200
-# docker container prune -f
+./evaluation/benchmarks/pyperf/scripts/run_infer.sh --model-config llm.eval_o4-mini --commit-hash HEAD --agent CodeActAgent --num-workers 80 --n-runs 2 --max-iter 200
+docker container prune -f
 
 # L = 400, k = 1, temp = N/A
-# ./evaluation/benchmarks/pyperf/scripts/run_infer.sh --model-config llm.eval_o4-mini --commit-hash HEAD --agent CodeActAgent --num-workers 60 --n-runs 1 --max-iter 400
-# docker container prune -f
+./evaluation/benchmarks/pyperf/scripts/run_infer.sh --model-config llm.eval_o4-mini --commit-hash HEAD --agent CodeActAgent --num-workers 60 --n-runs 1 --max-iter 400
+docker container prune -f
 
 # L = 800, k = 1, temp = N/A
 # ./evaluation/benchmarks/pyperf/scripts/run_infer.sh --model-config llm.eval_o4-mini --commit-hash HEAD --agent CodeActAgent --num-workers 60 --n-runs 1 --max-iter 800
