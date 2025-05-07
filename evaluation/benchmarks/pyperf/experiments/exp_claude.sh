c3bbb9710d37bd7d6b9b304c67a7e9004bfccb65
manishshettym <manishs@berkeley.edu>
2025-05-07 21:36:00 +0000

    minor updates

--- evaluation/benchmarks/pyperf/experiments/exp_claude.sh
+++ evaluation/benchmarks/pyperf/experiments/exp_claude.sh
@@ -9,20 +9,23 @@ set -eo pipefail
 # temp=0.k
 
 # L = 50, k = 8, temp = 0.8
-./evaluation/benchmarks/pyperf/scripts/run_infer.sh --model-config llm.eval_sonnet_vertex_0-8 --commit-hash HEAD --agent CodeActAgent --num-workers 50 --n-runs 8 --max-iter 50
-docker container prune -f
+# ./evaluation/benchmarks/pyperf/scripts/run_infer.sh --model-config llm.eval_sonnet_vertex_0-8 --commit-hash HEAD --agent CodeActAgent --num-workers 50 --n-runs 8 --max-iter 50
+# docker container prune -f
 
 # L = 100, k = 4, temp = 0.4
-./evaluation/benchmarks/pyperf/scripts/run_infer.sh --model-config llm.eval_sonnet_vertex_0-4 --commit-hash HEAD --agent CodeActAgent --num-workers 50 --n-runs 4 --max-iter 100
-docker container prune -f
+# until ./evaluation/benchmarks/pyperf/scripts/run_infer.sh --model-config llm.eval_sonnet_vertex_0-4 --commit-hash HEAD --agent CodeActAgent --num-workers 50 --n-runs 4 --max-iter 100; do
+#     echo "Error occurred. Retrying in 20 seconds..."
+#     sleep 20
+# done
+# docker container prune -f
 
 # L = 200, k = 2, temp = 0.2
-./evaluation/benchmarks/pyperf/scripts/run_infer.sh --model-config llm.eval_sonnet_vertex_0-2 --commit-hash HEAD --agent CodeActAgent --num-workers 50 --n-runs 2 --max-iter 200
-docker container prune -f
+# ./evaluation/benchmarks/pyperf/scripts/run_infer.sh --model-config llm.eval_sonnet_vertex_0-2 --commit-hash HEAD --agent CodeActAgent --num-workers 50 --n-runs 2 --max-iter 200
+# docker container prune -f
 
 # L = 400, k = 1, temp = 0.1
-./evaluation/benchmarks/pyperf/scripts/run_infer.sh --model-config llm.eval_sonnet_vertex_0-1 --commit-hash HEAD --agent CodeActAgent --num-workers 50 --n-runs 1 --max-iter 400
-docker container prune -f
+# ./evaluation/benchmarks/pyperf/scripts/run_infer.sh --model-config llm.eval_sonnet_vertex_0-1 --commit-hash HEAD --agent CodeActAgent --num-workers 50 --n-runs 1 --max-iter 400
+# docker container prune -f
 
 # L = 800, k = 1, temp = 0.1
 # ./evaluation/benchmarks/pyperf/scripts/run_infer.sh --model-config llm.eval_sonnet_vertex_0-1 --commit-hash HEAD --agent CodeActAgent --num-workers 50 --n-runs 1 --max-iter 800
@@ -36,5 +39,5 @@ docker container prune -f
 # L = 100
 # temp = 0.8
 
-# ./evaluation/benchmarks/pyperf/scripts/run_infer.sh --model-config llm.eval_sonnet_vertex --commit-hash HEAD --agent CodeActAgent --num-workers 40 --n-runs 10 --max-iter 100
+# ./evaluation/benchmarks/pyperf/scripts/run_infer.sh --model-config llm.eval_sonnet_vertex_0-8 --commit-hash HEAD --agent CodeActAgent --num-workers 40 --n-runs 10 --max-iter 100
 # docker container prune -f
